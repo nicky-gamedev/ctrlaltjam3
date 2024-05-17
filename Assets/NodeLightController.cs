eb2532b95b17d9a9c0f02f06b30e4c6a173d5e48
DevVand <61861793+DevVand@users.noreply.github.com>
2024-05-17 00:21:29 -0300

    ADD: scripts to node lights

---
+++ Assets/NodeLightController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class NodeLightController : MonoBehaviour
+{
+    NodeLight[] nodeLights;
+    [SerializeField] Light2D[] lights;
+    void Start()
+    {   
+        nodeLights = GetComponentsInChildren<NodeLight>();
+        
+        foreach (Light2D light in lights){
+            light.intensity = .05f;
+        }
+    }
+    public void TurnLightsOn(){
+        foreach (NodeLight nodeLight in nodeLights){
+            nodeLight.TurnLightOn();
+        }
+        foreach (Light2D light in lights){
+            DOTween.To(() => light.intensity, x => light.intensity = x, 1, 2);
+        }
+    }
+    public void Blink(){
+        foreach (NodeLight nodeLight in nodeLights){
+            nodeLight.Blink();
+        }
+        foreach (Light2D light in lights){
+            DOTween.To(() => light.intensity, x => light.intensity = x, 0, 5);
+        }
+    }
+}
